341e0d6dd7be96544289c10e899f71a2bd419f03
Mathieu Frenette <silphid@users.noreply.github.com>
2023-06-24 15:47:45 -0400

    Fix version increment on update

--- publish-people/go/pkg/publish/publish.go
+++ publish-people/go/pkg/publish/publish.go
@@ -39,14 +39,41 @@ type Storage struct {
 	Representation string `json:"representation"`
 }
 
+type PageResponse struct {
+	Version Version `json:"version"`
+}
+
 func Publish(opts Opts) error {
+	currentVersion, err := getCurrentVersion(opts)
+	if err != nil {
+		return fmt.Errorf("getting current version: %w", err)
+	}
+	return updatePage(opts, currentVersion+1)
+}
+
+func getCurrentVersion(opts Opts) (int, error) {
+	body := ""
+	response, err := sendRequest(opts, "GET", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return 0, fmt.Errorf("sending GET request: %w", err)
+	}
+
+	var pageResponse PageResponse
+	err = json.NewDecoder(bytes.NewReader(response)).Decode(&pageResponse)
+	if err != nil {
+		return 0, err
+	}
+	return pageResponse.Version.Number, nil
+}
+
+func updatePage(opts Opts, newVersion int) error {
 	file, err := os.ReadFile(opts.PageFile)
 	if err != nil {
 		return fmt.Errorf("reading page file: %w", err)
 	}
 
 	updateRequest := UpdatePageRequest{
-		Version: Version{Number: 2}, // Increase the version number to update the page
+		Version: Version{Number: newVersion},
 		Title:   opts.PageTitle,
 		Type:    "page",
 		Body: Body{
@@ -61,14 +88,24 @@ func Publish(opts Opts) error {
 		return fmt.Errorf("marshalling update request: %w", err)
 	}
 
-	// Create the HTTP request
-	endpoint := fmt.Sprintf("%s/wiki/rest/api/content/%s", opts.BaseUrl, opts.PageID)
-	req, err := http.NewRequest("PUT", endpoint, bytes.NewBuffer(requestBody))
+	// Update page
+	_, err = sendRequest(opts, "PUT", bytes.NewBuffer(requestBody))
 	if err != nil {
-		return fmt.Errorf("creating request: %w", err)
+		return fmt.Errorf("sending request: %w", err)
+	}
+	fmt.Println("Page updated successfully.")
+	return nil
+}
+
+func sendRequest(opts Opts, method string, bodyReader *bytes.Buffer) ([]byte, error) {
+	// Create HTTP request
+	endpoint := fmt.Sprintf("%s/wiki/rest/api/content/%s", opts.BaseUrl, opts.PageID)
+	req, err := http.NewRequest(method, endpoint, bodyReader)
+	if err != nil {
+		return nil, fmt.Errorf("creating request: %w", err)
 	}
 
-	// Set the request headers
+	// Set request headers
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(opts.Username, opts.ApiToken)
 
@@ -76,19 +113,17 @@ func Publish(opts Opts) error {
 	client := &http.Client{}
 	response, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("sending request: %w", err)
+		return nil, fmt.Errorf("sending request: %w", err)
 	}
 	defer response.Body.Close()
 
 	// Read response
 	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
-		return fmt.Errorf("reading response body: %w", err)
+		return nil, fmt.Errorf("reading response body: %w", err)
 	}
-	if response.StatusCode == http.StatusOK {
-		fmt.Println("Page updated successfully.")
-	} else {
-		return fmt.Errorf("unexpected response code: %d\n%s", response.StatusCode, responseBody)
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected response code: %d\n%s", response.StatusCode, responseBody)
 	}
-	return nil
+	return responseBody, nil
 }
